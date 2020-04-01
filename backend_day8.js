cf734c0b3dfe165d2c25d0ae10eb6deb2d336d6c
1stmuse <olysegs@gmail.com>
2020-04-01 17:54:21 +0100

    edited the code

--- backend_day8.js
+++ backend_day8.js
@@ -4,7 +4,10 @@ const convertNumToRoman = (num) =>{
     const numbers =[1000, 900, 500, 400,100, 90, 50, 40,10, 9, 5, 4, 1];
     let result = '';
 
+
     for(let i =0; i<romans.length; i++){
+        // if num is less than number[i], the while loop doesnt run, in goes back 
+        // into the for loop to increment i
         while( num >= numbers[i]){
             result += romans[i]
             num -= numbers[i]
